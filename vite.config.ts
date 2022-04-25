063798c61fbfc6075362824ecafd7a82061f98a6
Vincent Nabet <vnabet@groupeisagri.com>
2022-04-25 16:29:54 +0200

    sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from "path";
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    sourcemap: true,
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
